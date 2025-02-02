1fc7f873a17e91193324b0ef2ef01011b3dab1df
Akshay Patil <studyakshaystudy@outlook.com>
2025-02-02 18:09:54 +0800

    first commit

---
+++ src/main/java/com/posts/apidevelopment3party_posts/ApiDevelopment3partyPostsApplication.java
@@ -0,0 +1,13 @@
+package com.posts.apidevelopment3party_posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevelopment3partyPostsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDevelopment3partyPostsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posts/apidevelopment3party_posts/ApiDevelopment3partyPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posts.apidevelopment3party_posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopment3partyPostsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
